e37e40bc3755d3783cd605c20bffa6fe91d38f4f
Ian Flores <ianfloresfdez@gmail.com>
2022-10-13 19:53:53 +0200

    cambios finales programa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './main/game/game.component';
 import { MainComponent } from './main/main.component';
+
 GameComponent;
 const routes: Routes = [
   //ROUTE DECLARATION
--- src/app/main/game/game.component.spec.ts
+++ src/app/main/game/game.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GameComponent } from './game.component';
 
@@ -8,9 +10,9 @@ describe('GameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [GameComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,10 @@ describe('GameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a table with class board', () => {
+    const el = fixture.debugElement.query(By.css('table.board'));
+    expect(el).toBeTruthy();
   });
 });
